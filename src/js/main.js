6b0929dc84776f6556397e44b0ccde0c17b8030c
Waldemar Adams <waldek-adamski@wp.pl>
2020-06-19 22:59:37 +0200

    fn:catch text to localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,17 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const txtArea = document.querySelector('.txtarea--js')
+const btnLoad = document.querySelector('.load--js')
+const btnSave = document.querySelector('.save--js')
+
+btnLoad.addEventListener('click', () => {
+  txtArea.value = localStorage.getItem('txt')
+})
+
+btnSave.addEventListener('click', () => {
+  localStorage.setItem('txt', txtArea.value)
+})
+
 
 console.log(`Hello world!`);
-
-
